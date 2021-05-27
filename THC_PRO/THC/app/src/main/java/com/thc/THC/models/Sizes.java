a4e916f94a86471422682ecc8ad9e608e4fbcafd
Narendra babu <bnarendra340@gmail.com>
2021-05-27 21:26:23 +0530

    test file

--- THC_PRO/THC/app/src/main/java/com/thc/THC/models/Sizes.java
+++ THC_PRO/THC/app/src/main/java/com/thc/THC/models/Sizes.java
@@ -1,4 +1,37 @@
 package com.thc.THC.models;
 
 public class Sizes {
+    String id,size,stock,pid;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
 }
